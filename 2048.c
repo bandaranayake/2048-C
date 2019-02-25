6765a1202092fa2d027dfb995dd984563a5b36cd
Isuru Bandaranayake <isbana.git@gmail.com>
2019-02-25 11:47:53 +0000

    Code optimized

--- 2048.c
+++ 2048.c
@@ -98,25 +98,20 @@ int countEmpty(int grid[][4])
 
 void add(int grid[][4], int value)
 {
-	int t;
 	time_t ti;
-	while (1)
+	srand((unsigned)time(&ti));
+	int pos  = rand() % countEmpty(grid);
+	int count = 0;
+	for (int i = 0; i < 4; i++)
 	{
-		srand((unsigned)time(&ti));
-		int e = countEmpty(grid);
-		t = rand() % e;
-		int c = 0;
-		for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 4; j++)
 		{
-			for (int j = 0; j < 4; j++)
+			if (grid[i][j] == 0)
 			{
-				if (grid[i][j] == 0)
+				if (count++ == pos)
 				{
-					if (c++ == t)
-					{
-						grid[i][j] = value;
-						return;
-					}
+					grid[i][j] = value;
+					return;
 				}
 			}
 		}
@@ -180,9 +175,9 @@ int move(int grid[][4])
 	return score;
 }
 
-void rotate(int n, int grid[][4])
+void rotate(int rotations, int grid[][4])
 {
-	for (int r = 0; r < n; r++)
+	for (int r = 0; r < rotations; r++)
 	{
 		for (int i = 0; i < 2; i++)
 		{
@@ -198,37 +193,37 @@ void rotate(int n, int grid[][4])
 	}
 }
 
-int onKeyPress(char c, int grid[][4])
+int onKeyPress(char c, int grid[][4], int *score)
 {
 	if (c == 'q' || c == 'q')
 		return -1;
 	else if (c == 'r' || c == 'R')
-		return -2;
+		return 1;
 
 	int t = 0;
 	if (c == 'h' || c == 'H' || c == 'a' || c == 'A')
 	{
-		t = move(grid);
+		*score += move(grid);
 	}
 	else if (c == 'j' || c == 'J' || c == 's' || c == 'S')
 	{
 		rotate(3, grid);
-		t = move(grid);
+		*score += move(grid);
 		rotate(1, grid);
 	}
 	else if (c == 'k' || c == 'K' || c == 'w' || c == 'W')
 	{
 		rotate(1, grid);
-		t = move(grid);
+		score += move(grid);
 		rotate(3, grid);
 	}
 	else if (c == 'l' || c == 'L' || c == 'd' || c == 'D')
 	{
 		rotate(2, grid);
-		t = move(grid);
+		*score += move(grid);
 		rotate(2, grid);
 	}
-	return t;
+	return 0;
 }
 
 int check(int grid[][4])
@@ -239,7 +234,7 @@ int check(int grid[][4])
 		for (int j = 0; j < 4; j++)
 		{
 			if (grid[i][j] == 2048)
-				return 9;
+				return -1;
 			if (moveAvailable == 0)
 			{
 				if (grid[i][j] == 0)
@@ -258,42 +253,35 @@ int check(int grid[][4])
 	return moveAvailable;
 }
 
-int initialize(int grid[][4])
+void initialize(int grid[][4])
 {
 	for (int i = 0; i < 4; i++)
-	{
 		for (int j = 0; j < 4; j++)
 			grid[i][j] = 0;
-	}
 	add(grid, 2);
 	add(grid, 2);
-	return 0;
 }
 
 void menu()
 {
 	system("clear");
-	printf(" 2048 - Game\n * Use h-j-k-l / w-a-s-d keys to move the tiles.\n * When two tiles with the same number touch, they merge into one.\n\n");
-	printf("\t      ^      \t\t      ^\n\t      k      \t\t      w\n\t< h       l >\t\t< a       d >\n\t      j      \t\t      s\n\t      v      \t\t      v\n\n");
-	printf(" * Commands: \n\t r - Restart\n\t q - Exit\n");
-	printf("Press 'Enter' key to continue.. ");
-	char c;
-	scanf("%c", &c);
+	printf(" 2048 - Game\n * Use h-j-k-l / w-a-s-d keys to move the tiles.\n * When two tiles with the same number touch, they merge into one.\n\n\t      ^      \t\t      ^\n\t      k      \t\t      w\n\t< h       l >\t\t< a       d >\n\t      j      \t\t      s\n\t      v      \t\t      v\n\n * Commands: \n\t r - Restart\n\t q - Exit\nPress 'Enter' key to continue.. ");
+	getchar();
 }
 
 int getBestScore(char* path){
-	int i = 0;
+	int score = 0;
 	if(access( path, R_OK ) == 0){
 		FILE* file = fopen (path, "r");
-		fscanf (file, "%d", &i);    
+		fscanf (file, "%d", &score);    
 		fclose (file); 
 	}
-	return i;
+	return score;
 }
 
-void setBestScore(char* path, int i){
+void setBestScore(char* path, int score){
 	FILE* file = fopen (path, "w");
-	fprintf (file, "%d", i);    
+	fprintf (file, "%d", score);    
 	fclose (file); 
 }
 
@@ -301,34 +289,35 @@ void main()
 {
 	menu();
 	int grid[4][4];
-	int score = initialize(grid);
+	int score = 0;
 	int bestScore = getBestScore("data.txt");
+	initialize(grid);
+
 	while (1)
 	{
 		system("clear");
 		print(grid);
-
 		printf("\n BEST SCORE: %d\n SCORE: %d\n ", bestScore, score);
-		int t = onKeyPress(getchar(), grid);
-		if (t == -1)
+
+		int status = onKeyPress(getchar(), grid, &score);
+		if (status == -1)
 			break;
-		else if(t==-2){
-			score = initialize(grid);
+		else if(status == 1){
+			score = 0;
+			initialize(grid);
 			continue;
 		}
-		else
-			score += t;
 
 		if(score>bestScore) bestScore=score;
-		t = check(grid);
-		if (t == 9)
+		status= check(grid);
+		if (status == -1)
 		{
 			system("clear");
 			print(grid);
 			printf("\n BEST SCORE: %d\n Score: %d\n You Win !\n",bestScore, score);
 			break;
 		}
-		else if (t == 0)
+		else if (status == 0)
 		{
 			system("clear");
 			print(grid);
